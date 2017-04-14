1bd29e30349a9d6d3b47af4e6c9648b26754b621
mahmoud khudairi <mahmoudkhudairi@gmail.com>
2017-04-14 17:47:24 +0800

    Setting UI

---
+++ ClubsInstagram/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  ClubsInstagram
+//
+//  Created by mahmoud khudairi on 4/14/17.
+//  Copyright © 2017 mahmoud khudairi. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
