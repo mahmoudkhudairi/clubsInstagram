ee58284e81d89ad186de041f3b4efec0758ee643
mahmoud khudairi <mahmoudkhudairi@gmail.com>
2017-04-14 21:30:33 +0800

    Setting searchVC for showing the name of the users

--- ClubsInstagram/SearchVC.swift
+++ ClubsInstagram/SearchVC.swift
@@ -7,29 +7,57 @@
 //
 
 import UIKit
-
+import Firebase
 class SearchVC: UIViewController {
-
+var users = [User]()
+    @IBOutlet weak var friendsSearchBar: UISearchBar!
+    @IBOutlet weak var friendsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+fetchUser()
+        friendsTableView.dataSource = self
+        friendsTableView.delegate = self
+        
+    }
+    func fetchUser() {
+        FIRDatabase.database().reference().child("users").observe(.childAdded, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject] {
+                let user = User(dictionary: dictionary)
+                user.id = snapshot.key
+                
+                self.users.append(user)
+                
+               
+                DispatchQueue.main.async(execute: {
+                    self.friendsTableView.reloadData()
+                })
+                
+            }
+            
+        }, withCancel: nil)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+}
+extension SearchVC: UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         return users.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "friendsCell", for: indexPath)
+        print("Im here")
+        let user = users[indexPath.row]
+        cell.textLabel?.text = user.name
+        
+        if let profileImageUrl = user.profileImageUrl {
+          // cell.imageView?.image = UIImage(named: "uploadImage") //this was for testing it work
+            cell.imageView?.loadImageUsingCacheWithUrlString(profileImageUrl)//this is not showing images!!
+        }
+        
+        return cell
     }
-    */
-
+    
 }
